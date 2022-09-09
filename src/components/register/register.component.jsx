eeb811e72e9363425d3ea240a08a322407198a6a
VishalYadav24 <vishal.yadav@3pillarglobal.com>
2022-09-09 20:06:37 +0530

    added storage

--- src/components/register/register.component.jsx
+++ src/components/register/register.component.jsx
@@ -8,30 +8,91 @@ import {
   TextField,
 } from "@mui/material";
 import { Box, Stack } from "@mui/system";
-import React, { Fragment } from "react";
-import Image from "../../assets/annie-spratt-0ZPSX_mQ3xI-unsplash.jpg"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Image from "../../assets/annie-spratt-0ZPSX_mQ3xI-unsplash.jpg";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const handleSubmit = () => {
+    const User = {
+      name: userName,
+      email: userEmail,
+      password: userPassword,
+    };
+    try {
+      localStorage.setItem("User", JSON.stringify(User));
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <Box sx={{ height: "100vh",backgroundImage:`url(${Image})`,backgroundPosition:'center',backgroundSize:'cover' }} >
-      <Stack justifyContent="center" alignItems="center">
-        <Card sx={{margin:" calc(100vh - 80vh) auto"}}>
-          <CardHeader sx={{paddingTop:"32px"}} title="Register you account with us!"></CardHeader>
+    <Box
+      sx={{
+        height: "100vh",
+        backgroundImage: `url(${Image})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        component="form"
+        onSubmit={handleSubmit}
+      >
+        <Card sx={{ margin: " calc(100vh - 80vh) auto" }}>
+          <CardHeader
+            sx={{ paddingTop: "32px" }}
+            title="Register you account with us!"
+          ></CardHeader>
           <CardContent>
             <Stack spacing={2}>
-              <TextField required placeholder="Name"></TextField>
-              <TextField required placeholder="E-mail"></TextField>
-              <TextField required placeholder="Password"></TextField>
-              <Button variant="outlined" >REGISTER</Button>
+              <TextField
+                name="Name"
+                required
+                placeholder="Name"
+                value={userName}
+                onChange={(event) => {
+                  setUserName(event.target.value);
+                }}
+              ></TextField>
+              <TextField
+                name="Email"
+                required
+                placeholder="E-mail"
+                value={userEmail}
+                onChange={(event) => {
+                  setUserEmail(event.target.value);
+                }}
+              ></TextField>
+              <TextField
+                name="Password"
+                required
+                placeholder="Password"
+                value={userPassword}
+                onChange={(event) => {
+                  setUserPassword(event.target.value);
+                }}
+              ></TextField>
+              <Button variant="outlined" type="submit">
+                REGISTER
+              </Button>
             </Stack>
             <Divider sx={{ paddingTop: "16px" }} variant="fullWidth">
               or Continue with
             </Divider>
             <Box textAlign="center" paddingTop="16px" paddingBottom="32px">
               <Apple fontSize="large"></Apple>
-              <GitHub fontSize="large" ></GitHub>
-              <Google fontSize="large" ></Google>
-              <FacebookRounded fontSize="large" ></FacebookRounded>
+              <GitHub fontSize="large"></GitHub>
+              <Google fontSize="large"></Google>
+              <FacebookRounded fontSize="large"></FacebookRounded>
             </Box>
           </CardContent>
         </Card>
