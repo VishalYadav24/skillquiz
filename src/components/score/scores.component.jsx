1d73c08d0471218d1e5fe7cf1de470a6e9bfe8ea
VishalYadav24 <vishal.yadav@3pillarglobal.com>
2022-09-22 19:07:44 +0530

    separating custom styles

--- src/components/score/scores.component.jsx
+++ src/components/score/scores.component.jsx
@@ -15,27 +15,9 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Image from "../../assets/annie-spratt-0ZPSX_mQ3xI-unsplash.jpg";
+import  { CustomButton1,CustomButton2 } from "../custom-styles/custom.component";
 
-const CustomButton1 = styled(Button)({
-  borderColor: "#3cd458",
-  backgroundColor: "#fff",
-  ":hover": {
-    color: "#fff",
-    backgroundColor: "#3cd458",
-    borderColor: "#3cd458",
-    boxShadow: "0 1px 10px rgb(60 212 88 / 40%)",
-  },
-});
-const CustomButton2 = styled(Button)({
-  borderColor: "#fff",
-  backgroundColor: "#3cd458",
-  ":hover": {
-    color: "#3cd458",
-    backgroundColor: "#fff",
-    borderColor: "#3cd458",
-    boxShadow: "0 1px 10px rgb(60 212 88 / 40%)",
-  },
-});
+
 
 const Scores = ({ setUserAgreed, setRetry, setUserResponse,setQuestions }) => {
   const [userData, setUserData] = useState();
