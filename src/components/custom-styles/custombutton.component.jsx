41c24accb5526642dc608b919f52e5e66ddf4c03
VishalYadav24 <vishal.yadav@3pillarglobal.com>
2022-09-24 21:58:28 +0530

    added variable

--- src/components/custom-styles/custombutton.component.jsx
+++ src/components/custom-styles/custombutton.component.jsx
@@ -1,12 +1,13 @@
 import { Button } from '@mui/material'
 
-const CommonButton = ({children,variant,onClick,startIcon,endIcon,style}) => {
+const CommonButton = ({children,variant,onClick,startIcon,endIcon,style,type}) => {
   return (
     <Button
     variant={variant}
     onClick={onClick}
     startIcon={startIcon}
     endIcon={endIcon}
+    type={type}
     sx={{
         width: 300,
         color: 'blue',
