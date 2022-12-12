0b7f27954a493926e508360aa5736b7876b62d93
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2022-12-12 12:59:44 +0530

    removing unused packages

--- src/components/drawer/drawer.test.jsx
+++ src/components/drawer/drawer.test.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import ResponsiveDrawer from "./drawer.component";
-import toJson from 'enzyme-to-json';
 import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 describe("snapshot test",()=>{
